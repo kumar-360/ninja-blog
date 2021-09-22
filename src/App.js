98aa464bebfbacbd30963cac55e00e3fcbef2d33
Kumar Shanu <kumar.shanu@yourstory.com>
2021-09-22 11:06:25 +0530

    main wrapper added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AllBlogs from "./AllBlogs";
 import ErrorPage from "./ErrorPage";
+import MainWrapper from "./MainWrapper";
 import Navbar from "./Navbar";
 import NewBlog from "./NewBlog";
 import SingleBlog from "./SingleBlog";
@@ -9,26 +10,28 @@ import UpdateBlog from "./UpdateBlog";
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path='/'>
-          <AllBlogs />
-        </Route>
-        <Route exact path='/blogs/add-blog'>
-          <NewBlog />
-        </Route>
-        <Route exact path='/blogs/:id'>
-          <SingleBlog />
-        </Route>
-        <Route exact path='/blogs/update-blog/:id'>
-          <UpdateBlog />
-        </Route>
-        <Route path='*'>
-          <ErrorPage />
-        </Route>
-      </Switch>
-    </Router>
+    <MainWrapper>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <AllBlogs />
+          </Route>
+          <Route exact path='/blogs/add-blog'>
+            <NewBlog />
+          </Route>
+          <Route exact path='/blogs/:id'>
+            <SingleBlog />
+          </Route>
+          <Route exact path='/blogs/update-blog/:id'>
+            <UpdateBlog />
+          </Route>
+          <Route path='*'>
+            <ErrorPage />
+          </Route>
+        </Switch>
+      </Router>
+    </MainWrapper>
   );
 }
 
